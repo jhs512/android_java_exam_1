e9d9d09532f4cd8026a52e57df98bac04d307835
jhs512 <jangka512@gmail.com>
2021-02-21 15:45:13 +0900

    할일들(List<Todo>)을 sp에 저작

--- app/src/main/java/com/example/sbs/myapplication/ui/MainActivity.java
+++ app/src/main/java/com/example/sbs/myapplication/ui/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.sbs.myapplication.ui;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -10,6 +11,9 @@ import com.example.sbs.myapplication.util.Util;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import lombok.SneakyThrows;
 
 public class MainActivity extends AppCompatActivity {
@@ -18,13 +22,19 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //Todo todo = new Todo(1, "제목1");
+        Todo todo1 = new Todo(1, "제목1");
+        Todo todo2 = new Todo(2, "제목2");
 
-        //Util.spPut("todo1", todo);
-        //Util.spCommit();
+        List<Todo> todos = new ArrayList<>();
 
-        Todo todo = Util.spGetObj("todo1", Todo.class);
+        todos.add(todo1);
+        todos.add(todo2);
 
-        Util.toast(todo.toString());
+        Util.spPut("todos", todos);
+        Util.spCommit();
+
+        String jsonString = Util.spGetString("todos", "");
+
+        Util.log(jsonString);
     }
 }
