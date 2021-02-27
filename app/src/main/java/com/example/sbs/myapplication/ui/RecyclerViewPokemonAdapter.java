684c4e675f823a98812b1a765026c9fe196653ab
jhs512 <jangka512@gmail.com>
2021-02-27 17:38:04 +0900

    리사이클러 뷰에 헤더, 푸터 추가

--- app/src/main/java/com/example/sbs/myapplication/ui/RecyclerViewPokemonAdapter.java
+++ app/src/main/java/com/example/sbs/myapplication/ui/RecyclerViewPokemonAdapter.java
@@ -15,50 +15,98 @@ import com.example.sbs.myapplication.util.Util;
 
 import java.util.List;
 
-public class RecyclerViewPokemonAdapter extends RecyclerView.Adapter<RecyclerViewPokemonAdapter.ViewHolder> {
+public class RecyclerViewPokemonAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
+    private final int TYPE_HEADER = 0;
+    private final int TYPE_ITEM = 1;
+    private final int TYPE_FOOTER = 2;
+
     private List<Pokemon> data;
 
     public RecyclerViewPokemonAdapter(List<Pokemon> data) {
         this.data = data;
     }
 
+    @Override
+    public int getItemViewType(int position) {
+        if (position == 0)
+            return TYPE_HEADER;
+        else if (position == data.size() + 1)
+            return TYPE_FOOTER;
+        else
+            return TYPE_ITEM;
+    }
+
     @NonNull
     @Override
-    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        View view = LayoutInflater.from(parent.getContext())
-                .inflate(R.layout.item_pokemon, parent, false);
+    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        if (viewType == TYPE_HEADER) {
+            View view = LayoutInflater.from(parent.getContext())
+                    .inflate(R.layout.item_pokemon_header, parent, false);
 
-        return new ViewHolder(view);
+            return new HeaderViewHolder(view);
+        } else if (viewType == TYPE_FOOTER) {
+            View view = LayoutInflater.from(parent.getContext())
+                    .inflate(R.layout.item_pokemon_footer, parent, false);
+
+            return new FooterViewHolder(view);
+        } else {
+            View view = LayoutInflater.from(parent.getContext())
+                    .inflate(R.layout.item_pokemon, parent, false);
+
+            return new ItemViewHolder(view);
+        }
     }
 
     @Override
-    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
-        Pokemon pokemon = data.get(position);
-        holder.textViewId.setText(pokemon.getId() + "번");
-        holder.textViewId.setTag(position);
+    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
+        if (holder instanceof HeaderViewHolder) {
 
-        holder.textViewName.setText(pokemon.getName());
-        holder.textViewName.setTag(position);
-        
-        Util.loadImageOn(pokemon.getImgUrl(), holder.imageViewPokemon);
+        } else if (holder instanceof FooterViewHolder) {
+
+        } else {
+            ItemViewHolder itemViewHolder = (ItemViewHolder) holder;
+
+            int pokemonIndex = position - 1;
+
+            Pokemon pokemon = data.get(pokemonIndex);
+            itemViewHolder.textViewId.setText(pokemon.getId() + "번");
+            itemViewHolder.textViewId.setTag(pokemonIndex);
+
+            itemViewHolder.textViewName.setText(pokemon.getName());
+            itemViewHolder.textViewName.setTag(pokemonIndex);
+
+            Util.loadImageOn(pokemon.getImgUrl(), itemViewHolder.imageViewPokemon);
+        }
     }
 
     @Override
     public int getItemCount() {
-        return data.size();
+        return data.size() + 2;
     }
 
-    public static class ViewHolder extends RecyclerView.ViewHolder {
+    public static class ItemViewHolder extends RecyclerView.ViewHolder {
         public TextView textViewId;
         public TextView textViewName;
         public ImageView imageViewPokemon;
 
-        public ViewHolder(@NonNull View itemView) {
-            super(itemView);
+        public ItemViewHolder(@NonNull View view) {
+            super(view);
 
-            textViewId = itemView.findViewById(R.id.item_pokemon__textViewId);
-            textViewName = itemView.findViewById(R.id.item_pokemon__textViewName);
-            imageViewPokemon = itemView.findViewById(R.id.item_pokemon__imageViewPokemon);
+            textViewId = view.findViewById(R.id.item_pokemon__textViewId);
+            textViewName = view.findViewById(R.id.item_pokemon__textViewName);
+            imageViewPokemon = view.findViewById(R.id.item_pokemon__imageViewPokemon);
+        }
+    }
+
+    public static class HeaderViewHolder extends RecyclerView.ViewHolder {
+        public HeaderViewHolder(@NonNull View view) {
+            super(view);
+        }
+    }
+
+    public static class FooterViewHolder extends RecyclerView.ViewHolder {
+        public FooterViewHolder(@NonNull View view) {
+            super(view);
         }
     }
 }
