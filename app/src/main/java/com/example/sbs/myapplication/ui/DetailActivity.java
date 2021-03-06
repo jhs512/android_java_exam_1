d9a4d3ddce992ffb9fe17e6d09fff5ed57b3567d
jhs512 <jangka512@gmail.com>
2021-03-06 15:49:01 +0900

    activity_detail에 데이터바인딩 적용

--- app/src/main/java/com/example/sbs/myapplication/ui/DetailActivity.java
+++ app/src/main/java/com/example/sbs/myapplication/ui/DetailActivity.java
@@ -6,21 +6,20 @@ import android.os.Bundle;
 import android.widget.TextView;
 
 import com.example.sbs.myapplication.R;
+import com.example.sbs.myapplication.databinding.ActivityDetailBinding;
 import com.example.sbs.myapplication.util.Util;
 
 public class DetailActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_detail);
+        ActivityDetailBinding binding = ActivityDetailBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
 
         int id = getIntent().getIntExtra("id", 0);
 
         setTitle(id + "번 포켓몬 상세정보");
 
-        Util.log("id : " + id);
-
-        TextView textViewid = findViewById(R.id.activity_detail__textViewId);
-        textViewid.setText(id + "번");
+        binding.activityDetailTextViewId.setText(id + "번");
     }
 }
